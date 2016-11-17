0b5a2b0bd259eb315013d910e94da8f369ac5812
Richard Saylor <rsaylor@brightcove.com>
2016-11-17 13:42:32 -0500

    Added triggering syntax

--- DisplayCustomField.js
+++ DisplayCustomField.js
@@ -1,5 +1,7 @@
 videojs.plugin('name', 'options')
 	var player = videojs('myPlayer')
-		player.info = ('loadedmetadata')
-	info = player.mediainfo.custom_fields.name
+		this.ON = ('loadedmetadata', function() 
+			<<Display Custom Field>>
+		});
+	var info = player.mediainfo.custom_fields.name
 	
