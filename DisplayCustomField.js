15eba7ada975095d50b6bf042da1870ba45dfcf6
Richard Saylor <rsaylor@brightcove.com>
2016-11-18 14:35:38 -0500

    Fixed Syntax

--- DisplayCustomField.js
+++ DisplayCustomField.js
@@ -1,7 +1,8 @@
-videojs.plugin('name', 'options')
-	var player = this
-		this.ON = ('loadedmetadata', function() 
-			<<Display Title>>
-		});
-	var info = player.mediainfo.custom_fields.name
-	
+videojs.plugin('name', function() {
+	var player = this;
+	player.on = ('loadedmetadata', function(){
+		var info = player.mediainfo.custom_fields.name;
+		//This step would be taking the info variable and putting it to the ID of where you want it on the page//
+		spotOnPageID.innerHTML = info;
+	})
+});
